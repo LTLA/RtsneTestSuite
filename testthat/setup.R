3f3452a46fe0cf550b32f7f83114f07ec6bc2510
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-05 16:21:38 +0100

    Cut out Python middleman.

--- testthat/setup.R
+++ testthat/setup.R
@@ -1,34 +1,37 @@
-WRITEDATA <- function(data, fname=NULL) {
-    if (is.null(fname)) fname <- tempfile()
-    write.table(data, file=fname, sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
-    return(fname)
-}
-
 INITIALIZE <- function(nsamples, dims=2) {
-    X <- rnorm(nsamples*dims) * 0.0001
-    writeBin(con="init.dat", object=X)
-    invisible(NULL)
+    rnorm(nsamples*dims) * 0.0001
 }
 
 library(Rtsne)
-TESTFUN <- function(fname, perplexity=30, theta=0.5, max_iter=10, dims=2) {
-    incoming <- read.table(fname, header=FALSE)
-    Y_in <- readBin("init.dat", what="numeric", n=dims*nrow(incoming))
-    Y_in <- matrix(Y_in, nrow(incoming), ncol=dims, byrow=TRUE)
-    out <- Rtsne(as.matrix(incoming), perplexity=perplexity, theta=theta, Y_init=Y_in, 
-        dims=dims, max_iter=max_iter, pca=FALSE, stop_lying_iter=250L, mom_switch_iter=250L)
+TESTFUN <- function(data, init, perplexity=30, theta=0.5, max_iter=500, dims=2) {
+    Y_in <- matrix(init, nrow(data), ncol=dims, byrow=TRUE)
+    out <- Rtsne(data, perplexity=perplexity, theta=theta, Y_init=Y_in, 
+        dims=dims, max_iter=max_iter, pca=FALSE, stop_lying_iter=251L, mom_switch_iter=251L)
     out$Y    
 }
 
-REFFUN <- function(fname, perplexity=30, theta=0.5, max_iter=10, dims=2) 
-# Implements the reference function.
-{
-    outfile <- tempfile()
-    system2("../bhtsne/bhtsne.py", args=c(paste("-p", perplexity),
-        paste("-t", theta), paste("-m", max_iter), paste("-d", dims),
-        "--no_pca", paste("-i", fname), paste("-o", outfile)))
-    out <- as.matrix(read.table(outfile))
-    dimnames(out) <- NULL
-    out
+REFFUN <- function(data, init, perplexity=30, theta=0.5, max_iter=500, dims=2) {
+    infile <- "data.dat"
+    con <- file(infile, open="wb")
+    writeBin(nrow(data), con=con)
+    writeBin(ncol(data), con=con)
+    writeBin(as.numeric(theta), con=con)
+    writeBin(as.numeric(perplexity), con=con)
+    writeBin(as.integer(dims), con=con)
+    writeBin(as.integer(max_iter), con=con)
+    writeBin(as.numeric(t(data)), con=con)
+    close(con)
+
+    writeBin(con="init.dat", object=init)
+
+    system2("../bhtsne/bh_tsne", stdout=FALSE)
+
+    outfile <- "result.dat"
+    con <- file(outfile, open="rb")
+    n <- readBin(con=con, what="integer", 1)
+    d <- readBin(con=con, what="integer", 1)
+    out <- readBin(con=con, "numeric", dims*nrow(data))
+    close(con)
+    matrix(out, ncol=dims, byrow=TRUE)
 }
 
